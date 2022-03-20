b3f38286f5c253eb668cc44e4ccc076e0671bd66
Jodu555 <Jodu505@gmail.com>
2022-03-20 03:03:23 +0100

    Quick save to rollback

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -1,55 +1,27 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+	<v-app>
+		<v-toolbar>
+			Inventory Appp
+			<!-- <v-toolbar-title>Inventory App</v-toolbar-title> -->
+		</v-toolbar>
+		<!-- <v-app-bar absolute app>
+			<v-app-bar-title>Inventory App</v-app-bar-title>
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
+			<v-spacer></v-spacer>
+			<a to="/">Home</a>
+			<v-btn elevation="0"> Home </v-btn>
+		</v-app-bar> -->
 
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <router-view/>
-    </v-main>
-  </v-app>
+		<v-main>
+			<v-container> Hello, World </v-container>
+		</v-main>
+	</v-app>
 </template>
 
 <script>
-
 export default {
-  name: 'App',
+	name: 'App',
 
-  data: () => ({
-    //
-  }),
+	data: () => ({}),
 };
 </script>
