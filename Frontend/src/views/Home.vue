bb7e535b52ceeac48da7f246ecf7d1065c4ea7d9
Jodu555 <Jodu505@gmail.com>
2022-03-21 02:50:26 +0100

    Some updates

--- Frontend/src/views/Home.vue
+++ Frontend/src/views/Home.vue
@@ -75,7 +75,37 @@
 					hide-details
 					class="mr-10"
 				></v-text-field>
-				<v-btn color="primary"> Neuer Eintrag </v-btn>
+				<v-dialog v-model="dialog" max-width="500px">
+					<template v-slot:activator="{ on, attrs }">
+						<v-btn color="primary" v-bind="attrs" v-on="on"> Neuer Eintrag </v-btn>
+					</template>
+					<v-card>
+						<v-card-title>
+							<span class="text-h5">{{ formTitle }}</span>
+						</v-card-title>
+						<v-card-text>
+							<v-container>
+								<v-row>
+									<v-col cols="12" sm="6" md="4">
+										<v-text-field v-model="editedItem.name" label="Name"></v-text-field>
+									</v-col>
+									<v-col cols="12" sm="6" md="4">
+										<v-text-field v-model="editedItem.chest" label="Kiste"></v-text-field>
+									</v-col>
+									<v-col cols="12" sm="6" md="4">
+										<v-text-field v-model="editedItem.amount" label="Stückzahl"></v-text-field>
+									</v-col>
+								</v-row>
+							</v-container>
+						</v-card-text>
+
+						<v-card-actions>
+							<v-spacer></v-spacer>
+							<v-btn color="blue darken-1" text @click="close"> Cancel </v-btn>
+							<v-btn color="blue darken-1" text @click="save"> Save </v-btn>
+						</v-card-actions>
+					</v-card>
+				</v-dialog>
 			</v-layout>
 		</v-card-title>
 		<v-data-table :headers="headers" :items="items" :search="search">
@@ -123,10 +153,31 @@ export default {
 			item.amount--;
 			this.updateItem(item);
 		},
+		save() {},
+		close() {
+			this.dialog = false;
+			this.$nextTick(() => {
+				this.editedItem = JSON.parse(JSON.stringify(this.defaultItem));
+				this.editItemUUID = -1;
+			});
+		},
+		editItem(item) {
+			this.editItemUUID = item.UUID;
+			this.editedItem = Object.assign({}, item);
+			this.dialog = true;
+		},
+	},
+	computed: {
+		formTitle() {
+			return this.formType === -1 ? 'neuer Eintrag' : 'Eintrag Bearbeiten';
+		},
 	},
 	data() {
 		return {
 			search: '',
+			editItemUUID: -1,
+			dialog: false,
+			dialogDelete: false,
 			headers: [
 				{
 					text: 'Name',
@@ -138,11 +189,19 @@ export default {
 				{ text: 'Anzahl', value: 'amount' },
 				{ text: 'Aktion', value: 'actions' },
 			],
+			editedItem: {},
+			defaultItem: {
+				name: '',
+				chest: 1,
+				amount: 1,
+			},
+
 			items: [],
 		};
 	},
 
 	created() {
+		this.editItem = JSON.parse(JSON.stringify(this.defaultItem));
 		this.fetchData();
 	},
 };
@@ -151,19 +210,6 @@ export default {
 // 	data: () => ({
 // 		dialog: false,
 // 		dialogDelete: false,
-// 		headers: [
-// 			{
-// 				text: 'Dessert (100g serving)',
-// 				align: 'start',
-// 				sortable: false,
-// 				value: 'name',
-// 			},
-// 			{ text: 'Calories', value: 'calories' },
-// 			{ text: 'Fat (g)', value: 'fat' },
-// 			{ text: 'Carbs (g)', value: 'carbs' },
-// 			{ text: 'Protein (g)', value: 'protein' },
-// 			{ text: 'Actions', value: 'actions', sortable: false },
-// 		],
 // 		desserts: [],
 // 		editedIndex: -1,
 // 		editedItem: {
@@ -181,11 +227,6 @@ export default {
 // 			protein: 0,
 // 		},
 // 	}),
-// 	computed: {
-// 		formTitle() {
-// 			return this.editedIndex === -1 ? 'New Item' : 'Edit Item';
-// 		},
-// 	},
 // 	watch: {
 // 		dialog(val) {
 // 			val || this.close();
@@ -198,80 +239,7 @@ export default {
 // 		this.initialize();
 // 	},
 // 	methods: {
-// 		initialize() {
-// 			this.desserts = [
-// 				{
-// 					name: 'Frozen Yogurt',
-// 					calories: 159,
-// 					fat: 6.0,
-// 					carbs: 24,
-// 					protein: 4.0,
-// 				},
-// 				{
-// 					name: 'Ice cream sandwich',
-// 					calories: 237,
-// 					fat: 9.0,
-// 					carbs: 37,
-// 					protein: 4.3,
-// 				},
-// 				{
-// 					name: 'Eclair',
-// 					calories: 262,
-// 					fat: 16.0,
-// 					carbs: 23,
-// 					protein: 6.0,
-// 				},
-// 				{
-// 					name: 'Cupcake',
-// 					calories: 305,
-// 					fat: 3.7,
-// 					carbs: 67,
-// 					protein: 4.3,
-// 				},
-// 				{
-// 					name: 'Gingerbread',
-// 					calories: 356,
-// 					fat: 16.0,
-// 					carbs: 49,
-// 					protein: 3.9,
-// 				},
-// 				{
-// 					name: 'Jelly bean',
-// 					calories: 375,
-// 					fat: 0.0,
-// 					carbs: 94,
-// 					protein: 0.0,
-// 				},
-// 				{
-// 					name: 'Lollipop',
-// 					calories: 392,
-// 					fat: 0.2,
-// 					carbs: 98,
-// 					protein: 0,
-// 				},
-// 				{
-// 					name: 'Honeycomb',
-// 					calories: 408,
-// 					fat: 3.2,
-// 					carbs: 87,
-// 					protein: 6.5,
-// 				},
-// 				{
-// 					name: 'Donut',
-// 					calories: 452,
-// 					fat: 25.0,
-// 					carbs: 51,
-// 					protein: 4.9,
-// 				},
-// 				{
-// 					name: 'KitKat',
-// 					calories: 518,
-// 					fat: 26.0,
-// 					carbs: 65,
-// 					protein: 7,
-// 				},
-// 			];
-// 		},
+//
 // 		editItem(item) {
 // 			this.editedIndex = this.desserts.indexOf(item);
 // 			this.editedItem = Object.assign({}, item);
